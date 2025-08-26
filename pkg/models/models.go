be29ae43d81d003230806e4ae1710d24dd8847c1
Ryan Harris <ryan@harrisit.co.uk>
2025-08-26 08:35:31 +0100

    removed excessive file

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -124,3 +124,13 @@ type UserOAuthParams struct {
 	OauthProvider string `json:"oauthProvider"`
 	OauthID       string `json:"oauthId"`
 }
+
+// Session represents the data stored for a single user session.
+type Session struct {
+	ID        string    // Unique identifier for the session (e.g., UUID)
+	UserID    uuid.UUID // ID of the user associated with this session
+	ExpiresAt time.Time // When the session becomes invalid
+	CreatedAt time.Time // When the session was created
+	IpAddress *string   // Optional ip address
+	UserAgent *string   // Optional UserAgent
+}
